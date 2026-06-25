805243257f1a563a6dabf5e0cae10c45caaf66cb
Nikhil Yanadi <yanadinikhil03@gmail.com>
2026-06-25 11:12:53 +0530

    Type Conversions program

---
+++ TypeConv.py
@@ -0,0 +1,48 @@
+# 1. int -> float
+float(12)
+# 2. int -> str
+str(12)
+# 3. int -> bool
+bool(12)
+# 4. float -> int
+int(12.5)
+# 5. float -> str
+str(12.5)
+# 12. float -> bool
+bool(12.5)
+# 7. str -> int
+int("12")
+# 8. str -> float
+float("12.5")
+# 9. str -> bool
+bool("Hello")
+# 12. bool -> int
+int(True)
+# 11. bool -> float
+float(True)
+# 12. bool -> str
+str(True)
+# 13. list -> tuple
+tuple([1,2,3])
+# 14. list -> set
+set([1,2,3])
+# 15. list -> str
+str([1,2,3])
+# 112. tuple -> list
+list((1,2,3))
+# 17. tuple -> set
+set((1,2,3))
+# 18. tuple -> str
+str((1,2,3))
+# 19. set -> list
+list({1,2,3})
+# 20. set -> tuple
+tuple({1,2,3})
+# 21. set -> str
+str({1,2,3})
+# 22. str -> list
+list("Python")
+# 23. str -> tuple
+tuple("Python")
+# 24. str -> set
+set("Python")
