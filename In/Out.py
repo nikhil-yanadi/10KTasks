29b2dac82725df1860ef0d3d28bf84cff337117a
Nikhil Yanadi <yanadinikhil03@gmail.com>
2026-06-25 10:50:15 +0530

    Input output Error's

---
+++ In/Out.py
@@ -0,0 +1,21 @@
+# SyntaxError
+if True
+    print("Hello")
+
+# NameError
+print(x)
+
+# TypeError
+a=10
+b="5"
+print(a+b)
+
+# ValueError
+n=int("hello")
+
+# EOFError
+name=input("Enter name: ")
+
+# IndentationError
+if True:
+    print("Hello")
