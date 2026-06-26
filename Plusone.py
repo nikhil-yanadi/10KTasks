7ed437d3ca2f1435b8c5650d7ed4043ff8e1e110
Nikhil Yanadi <yanadinikhil03@gmail.com>
2026-06-26 10:32:08 +0530

    Plus one program

---
+++ Plusone.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        s=0
+        for i in range(len(digits)):
+            s=s*10+digits[i]
+        s=s+1
+        return [int(x) for x in str(s)]
+        
