d9f85a9f3189f8c5f8c7de313ee94ef71c606db5
Nikhil Yanadi <yanadinikhil03@gmail.com>
2026-06-27 10:18:47 +0530

    XOR Program

---
+++ XOR.py
@@ -0,0 +1,41 @@
+a=3
+b=4
+print(a^b)
+a=5
+b=3
+print(a&b)
+a=5
+b=3
+print(a|b)
+
+b=5
+print(~b)
+
+a=3
+print(a<<1)
+
+a=5
+print(a>>1)
+
+n = int(input("Enter a Number:"))
+if n & 1:
+    print("odd")
+else:
+    print("even")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
